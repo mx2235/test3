5901ef3e5d353827525d5370156f39bb55eecec8
lemonma <mx2235@126.com>
2017-06-12 10:55:01 +0800

    Initial commit

---
+++ src/com/mx/Test3.java
@@ -0,0 +1,13 @@
+package com.mx;
+
+/**
+ * Created by DT321 on 2017/6/12.
+ */
+public class Test3
+
+{
+
+    public static void main(String[] args) {
+        System.out.print("test3");
+    }
+}
